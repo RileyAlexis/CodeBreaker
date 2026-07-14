8de856d3d0c3f46643b6b8e96307933891d18634
RileyAlexis <MxRileyAlexis@gmail.com>
2026-07-14 09:36:31 -0500

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by RileyAlexis on 7/14/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
